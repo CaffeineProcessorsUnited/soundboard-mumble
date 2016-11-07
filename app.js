a68a696244642841e9fd58e88835f550b6db0093
Felix5721 <felix5_721-github@yahoo.de>
2016-11-07 15:45:25 +0100

    now reads config to know to wich mumble server to connect, also now listens to chat messages and output them

--- app.js
+++ app.js
@@ -1,24 +1,57 @@
 var mumble = require('mumble');
+var request = require('request');
+var fs = require('fs');
+var io = require('socket.io-client');
+var cpu = require("./soundboard-lib/cpu.js");
 
-var options = {
-    //key: fs.readFileSync( 'key.pem' ),
-    //cert: fs.readFileSync( 'cert.pem' )
-};
+//Loading CPU Modules
+cpu.loadModule(require("./soundboard-lib/util.cpu.js"), { "utils": require('util')});
+cpu.loadModule(require("./soundboard-lib/events.cpu.js"));
+cpu.loadModule(require("./soundboard-lib/socket.cpu.js"), { "io" : io, "server": false });
+cpu.loadModule(require("./soundboard-lib/config.cpu.js"));
 
-console.log( 'Connecting' );
-mumble.connect( 'mumble://mygamehub.de', options, function ( error, connection ) {
-    if( error ) { throw new Error( error ); }
-    console.log( 'Connected' );
-    connection.authenticate( 'SoundboardJudge' );
-    connection.on( 'initialized', onInit );
-    connection.on( 'voice', onVoice );
+console.log(cpu);
+//Configure CPU
+cpu.module("events").addEventListener("ready", function(cpu) {
+  cpu.module("config").load(JSON.parse(fs.readFileSync('config.json', 'utf8')));
 });
 
-var onInit = function() {
-    console.log( 'Connection initialized' );
-    // Connection is authenticated and usable.
-};
+cpu.ready();
 
-var onVoice = function( voice ) {
-    console.log( "Recieving Voice Data" );
-};
+var options = {
+  "key" : cpu.module("config").get("mumble-client", "key"),
+  "cert": cpu.module("config").get("mumble-client", "cert")
+}
+
+var hostname = cpu.module("config").get("mumble-server", "hostname");
+if (hostname != null && hostname != "") {
+  mumble.connect( "mumble://" + hostname, options, function ( error, connection ) {
+      if( error ) { throw new Error( error ); }
+      connection.authenticate( 'SoundboardJudge' );
+      connection.on( 'initialized', function(){
+        var streamtoserver = connection.inputStream({
+           sampleRate: 44100,
+           channels: 2
+        });
+        var url = "http://" + cpu.module("config").get("soundboard-server", "hostname") + ":" + cpu.module("config").get("soundboard-server", "stream-port");
+        r = request.get(url);
+        r.on('response', function (response) {
+          response.on('data', function (chunk) {
+            try {
+              streamtoserver.write(chunk);
+            } catch(e) {
+              console.error(e);
+            }
+          });
+          response.on('end', function () {
+            cpu.module("util").log('Stream ended');
+          });
+        });
+      });
+      connection.on("message", function(message, user, scope){
+        cpu.module("util").log("Mumble: message \"" + message + "\"");
+      });
+  });
+} else {
+  cpu.module("util").log("Cannot connect to mumble server: No Hostname provided in config file");
+}
